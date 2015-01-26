5d379f13a807c90e2feb0fbf7a4287fc4e8715fc
unknown <2092971R@BO715-2-02.ad.dcs.gla.ac.uk>
2015-01-26 15:42:57 +0000

    tangowithdjango

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,5 +7,5 @@ def index(request):
     return HttpResponse( 'Rango says: Hello world!')
 
 def about(request):
-	return HttpResponse( "Rango sayd this is the about page" )
+	return HttpResponse( "Rango says this is the about page, this tutorial has been put together by Alexandra Russell, 2092971R")
 

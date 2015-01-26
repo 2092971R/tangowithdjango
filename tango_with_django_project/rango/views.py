17e8e41bf5a5e3b0d51a6887d63412b17c5bd002
unknown <2092971R@BO715-2-02.ad.dcs.gla.ac.uk>
2015-01-26 15:26:21 +0000

    Chapter 4 - completed

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse( 'Rango says: Hello world!')
+
+def about(request):
+	return HttpResponse( "Rango sayd this is the about page" )
+
